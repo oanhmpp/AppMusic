cc4cf69df06d72f03440e5ece91b782499d0ceb2
oanhmpp <oanhmpp@gmail.com>
2020-12-21 13:16:05 +0700

    chi7bo

---
+++ app/src/main/java/com/example/appmusic/Adapter/BannerAdapter.java
@@ -0,0 +1,78 @@
+package com.example.appmusic.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.appmusic.Model.Advertisement;
+import com.example.appmusic.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class BannerAdapter  extends PagerAdapter {
+    Context context;
+    ArrayList<Advertisement> arrayListBanner; //mãng quảng cáo
+
+    public BannerAdapter(Context context, ArrayList<Advertisement> arrayListBanner) {
+        this.context = context;
+        this.arrayListBanner = arrayListBanner;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayListBanner.size();
+    }
+//trả về view tùy theo object định hình
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    //định hình object (mỗi object tương trưng cho mỗi page
+    //Nhìn view mẫu sẽ đi thiết kế cho những page còn lại
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater=LayoutInflater.from(context);
+        View view=layoutInflater.inflate(R.layout.line_banner,null);
+        //ánh xạ view trong phần banner vào
+        ImageView imageViewBackgroundBanner=view.findViewById(R.id.imageViewBackgroundBanner);
+        ImageView imageSongBanner=view.findViewById(R.id.imageViewBanner);
+        TextView txtTitleSongBanner=view.findViewById(R.id.textviewTitleSongBanner);
+        TextView txtContent=view.findViewById(R.id.textviewindex);
+        //gắn giá trị
+        Picasso.with(context).load(arrayListBanner.get(position).getImageAdver()).into(imageViewBackgroundBanner);
+        Picasso.with(context).load(arrayListBanner.get(position).getImageSong()).into(imageSongBanner);
+        txtTitleSongBanner.setText(arrayListBanner.get(position).getNameSong());
+        txtContent.setText(arrayListBanner.get(position).getContentAdver());
+
+//        // sự kiện khi nhấn vào page trên quảng cáo
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Toast.makeText(context, "Bạn đã click", Toast.LENGTH_LONG).show();
+//                // chuyển và gửi dữ liệu qua màn hình danh sách các bài hát
+//                Intent intent = new Intent(context, DanhSachBaiHatActivity.class);
+//                intent.putExtra("banner", arrayListBanner.get(position));
+//                context.startActivity(intent);
+//            }
+//        });
+
+        //add vào viewpager
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
---
+++ app/src/main/java/com/example/appmusic/Fragment/FragmentBanner.java
@@ -0,0 +1,91 @@
+package com.example.appmusic.Fragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.appmusic.Adapter.BannerAdapter;
+import com.example.appmusic.Model.Advertisement;
+import com.example.appmusic.R;
+import com.example.appmusic.Service.APIServer;
+import com.example.appmusic.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FragmentBanner extends Fragment {
+    View view;
+    ViewPager viewPager;
+    CircleIndicator circleIndicator;
+    BannerAdapter bannerAdapter;
+    Runnable runnable;
+    Handler handler;
+    int currentItem;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_banner,container,false);
+        mapping();
+        getData();
+        return view;
+    }
+
+    private void mapping() {
+        viewPager=view.findViewById(R.id.viewPager);
+        circleIndicator=view.findViewById(R.id.indicatorDefault);
+    }
+
+    public void getData(){
+        DataService dataService= APIServer.getService();
+        Call<List<Advertisement>> callback =dataService.getDataBanner();
+        callback.enqueue(new Callback<List<Advertisement>>() {
+            @Override
+            public void onResponse(Call<List<Advertisement>> call, Response<List<Advertisement>> response) {
+                ArrayList<Advertisement> banners= (ArrayList<Advertisement>) response.body();
+//                Log.d("BBB",banners.get(0).getIDAdver());
+                bannerAdapter=new BannerAdapter(getActivity(),banners);
+                viewPager.setAdapter(bannerAdapter);
+                circleIndicator.setViewPager(viewPager);
+
+                handler=new Handler();
+                runnable=new Runnable() {
+                    @Override
+                    public void run() {
+                        //kiểm tra item hiện tại
+                        currentItem=viewPager.getCurrentItem();
+                        //tăng vị trí đang đứng
+                        currentItem++;
+                        //vượt kích thước thì trở về page đầu tiên
+                        if(currentItem>=viewPager.getAdapter().getCount()){
+                            currentItem=0;
+                        }
+                        //mỗi lần chạy xong set dữ liệu lên
+                        viewPager.setCurrentItem(currentItem,true);
+                        //thời gian chờ 4 giây
+                        handler.postDelayed(runnable,4000);
+
+                    }
+                };
+                handler.postDelayed(runnable,4000);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Advertisement>> call, Throwable t) {
+
+            }
+        });
+    }
+}

d8ee53dac78f9cb7f79362e519d78e3cc2101010
hang2705 <59534665+hang2705@users.noreply.github.com>
2020-12-29 14:38:08 +0700

    Merge branch 'main' of C:\Users\Admin\Downloads\AppMusic-28-12-2020\AppMusic-28-12-2020 with conflicts.

--- app/src/main/java/com/example/appmusic/Model/PlayList.java
+++ app/src/main/java/com/example/appmusic/Model/PlayList.java
@@ -5,49 +5,49 @@ import com.google.gson.annotations.SerializedName;
 
 public class PlayList {
 
-@SerializedName("IDPlayList")
-@Expose
-private String iDPlayList;
-@SerializedName("NamePlayList")
-@Expose
-private String namePlayList;
-@SerializedName("Background")
-@Expose
-private String background;
-@SerializedName("ImageIcon")
-@Expose
-private String imageIcon;
+    @SerializedName("IDPlayList")
+    @Expose
+    private String iDPlayList;
+    @SerializedName("NamePlayList")
+    @Expose
+    private String namePlayList;
+    @SerializedName("Background")
+    @Expose
+    private String background;
+    @SerializedName("ImageIcon")
+    @Expose
+    private String imageIcon;
 
-public String getIDPlayList() {
-return iDPlayList;
-}
+    public String getIDPlayList() {
+        return iDPlayList;
+    }
 
-public void setIDPlayList(String iDPlayList) {
-this.iDPlayList = iDPlayList;
-}
+    public void setIDPlayList(String iDPlayList) {
+        this.iDPlayList = iDPlayList;
+    }
 
-public String getNamePlayList() {
-return namePlayList;
-}
+    public String getNamePlayList() {
+        return namePlayList;
+    }
 
-public void setNamePlayList(String namePlayList) {
-this.namePlayList = namePlayList;
-}
+    public void setNamePlayList(String namePlayList) {
+        this.namePlayList = namePlayList;
+    }
 
-public String getBackground() {
-return background;
-}
+    public String getBackground() {
+        return background;
+    }
 
-public void setBackground(String background) {
-this.background = background;
-}
+    public void setBackground(String background) {
+        this.background = background;
+    }
 
-public String getImageIcon() {
-return imageIcon;
-}
+    public String getImageIcon() {
+        return imageIcon;
+    }
 
-public void setImageIcon(String imageIcon) {
-this.imageIcon = imageIcon;
-}
+    public void setImageIcon(String imageIcon) {
+        this.imageIcon = imageIcon;
+    }
 
 }
